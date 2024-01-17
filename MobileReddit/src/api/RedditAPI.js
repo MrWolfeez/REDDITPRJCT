be053b8f49780b1b847ba44c785018954eee1237
MrWolfeez <louis.lescure@epitech.eu>
2024-01-17 11:51:23 +0100

    redditAPI

--- MobileReddit/src/api/RedditAPI.js
+++ MobileReddit/src/api/RedditAPI.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const redditAxios = axios.create({
+  baseURL: 'https://oauth.reddit.com',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+});
+
 const fetchPopularSubreddits = async () => {
   try {
     const response = await axios.get('https://www.reddit.com/subreddits/popular.json');
@@ -10,4 +15,40 @@ const fetchPopularSubreddits = async () => {
   }
 };
 
-export { fetchPopularSubreddits };
+const getSubscribedSubreddits = async (accessToken) => {
+  try {
+    const response = await redditAxios.get('/subreddits/mine/subscriber', {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    return response.data.data.children.map(subreddit => subreddit.data);
+  } catch (error) {
+    console.error('error :', error);
+  }
+};
+
+const joinSubreddit = async (accessToken, subredditId) => {
+  try {
+    const response = await redditAxios.post('/api/subscribe', {
+      action: 'sub',
+      sr: subredditId
+    }, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('error :', error);
+  }
+};
+
+const searchSubreddits = async (accessToken, query) => {
+  try {
+    const response = await redditAxios.get(`/subreddits/search?q=${encodeURIComponent(query)}`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    return response.data.data.children.map(subreddit => subreddit.data);
+  } catch (error) {
+    console.error('error :', error);
+  }
+};
+
+export { fetchPopularSubreddits, getSubscribedSubreddits, joinSubreddit, searchSubreddits };
